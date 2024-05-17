e1efb94414ee660221af33bafa5bade5c7dc7220
sodam.hwang <sodam.hwang@samsung.com>
2024-05-17 14:43:22 +0900

    refact baseball class

--- baseball/Baseball.cpp
+++ baseball/Baseball.cpp
@@ -6,13 +6,20 @@ using namespace std;
 class Baseball
 {
 public:
+	bool checkDuplicateNumber(const string& guessNumber)
+	{
+		return (guessNumber[0] == guessNumber[1] ||
+			guessNumber[1] == guessNumber[2] ||
+			guessNumber[0] == guessNumber[2]);
+	}
+
 	void guess(const string& guessNumber)
 	{
 		if (guessNumber.length() != 3)
 		{
 			throw length_error("Must be three letters!");
 		}
-			
+
 		for (char ch : guessNumber)
 		{
 			if (ch < '0' || ch > '9')
@@ -21,11 +28,10 @@ public:
 			}
 		}
 
-		if (guessNumber[0] == guessNumber[1] || 
-			guessNumber[1] == guessNumber[2] || 
-			guessNumber[0] == guessNumber[2])
+		if (checkDuplicateNumber(guessNumber))
 		{
 			throw invalid_argument("Must not have same number!");
 		}
 	}
 };
+
